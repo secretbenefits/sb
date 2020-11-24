6bd81d61985910ab913bcff8982c9a6e70e68b19
tom <dhagher@gmail.com>
2020-11-24 16:55:56 +0100

    Use memory efficient binstr representation for data

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Sb
 
-## `hamming_filter(array, filter, distance, limit)`
+## `hamming_filter(binstr, filter, distance, limit)`
 
-Filter `array` returning at most `limit` values within hamming `distance` from provided `filter` value.
+Filter `binstr` returning array of at most `limit` values within hamming `distance` from provided `filter` value.
+Provided `binstr` must be an array of numbers packed using `Q` modifier, eg `[1, 2, 3].pack('Q*')`.
--- ext/sb_ext/sb_ext.c
+++ ext/sb_ext/sb_ext.c
@@ -1,27 +1,25 @@
 #include "ruby.h"
 
-VALUE Sb = Qnil;
-
-VALUE hamming_filter(VALUE _self, VALUE _array, VALUE _filter, VALUE _distance, VALUE _limit) {
-  long row;
-  long rows = RARRAY_LEN(_array);
-  unsigned long long filter = NUM2ULL(_filter);
-  long distance = NUM2ULONG(_distance);
-  long limit = NUM2ULONG(_limit);
+VALUE hamming_filter(VALUE _self, VALUE _bin_str, VALUE _filter, VALUE _distance, VALUE _limit) {
+  const uint64_t filter = NUM2ULL(_filter);
+  const long distance = NUM2ULONG(_distance);
+  const long limit = NUM2ULONG(_limit);
   long result_num = 0;
+  uint64_t *value = (uint64_t *)RSTRING_PTR(_bin_str);
+  const long value_num = RSTRING_LEN(_bin_str) / sizeof(uint64_t);
+  const uint64_t *last = value + value_num;
   VALUE result = rb_ary_new_capa(limit);
 
-  Check_Type(_array, T_ARRAY);
+  Check_Type(_bin_str, T_STRING);
   Check_Type(_distance, T_FIXNUM);
   Check_Type(_limit, T_FIXNUM);
 
   if (limit < 1)
     return result;
 
-  for (row = 0; row < rows; row++) {
-    VALUE value = rb_ary_entry(_array, row);
-    if (__builtin_popcountll(filter ^ NUM2ULL(value)) <= distance) {
-      rb_ary_store(result, result_num, value);
+  for (; value < last; value++) {
+    if (__builtin_popcountll(filter ^ *value) <= distance) {
+      rb_ary_store(result, result_num, ULL2NUM(*value));
       if (++result_num >= limit)
         break;
     }
@@ -31,6 +29,6 @@ VALUE hamming_filter(VALUE _self, VALUE _array, VALUE _filter, VALUE _distance, 
 }
 
 void Init_sb_ext() {
-  Sb = rb_const_get(rb_cObject, rb_intern("Sb"));
+  VALUE Sb = rb_const_get(rb_cObject, rb_intern("Sb"));
   rb_define_module_function(Sb, "hamming_filter", hamming_filter, 4);
 }
